3fac4822192f0f2ed055307bdbe8831cc0496de2
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-06-17 18:25:12 -0500

    cambiando posiciones y función de movimientos

--- min-max.py
+++ min-max.py
@@ -1,69 +1,60 @@
 from nodo import Nodo
-
+from views.tablero import Tablero
 import numpy as np
 
 def generar_tablero():
-    tablero = np.zeros((8, 8), dtype=int)
+        tablero = np.zeros((8, 8), dtype=int)
 
-    # Generar una lista de números del 1 al 9
-    numeros = list(range(1, 10))
+        # Generar una lista de números del 1 al 7 (excluyendo el 8 y 9)
+        numeros = list(range(1, 8))
 
-    # Asignar los números en posiciones aleatorias del tablero sin superposición
-    for num in numeros:
+        # Asignar los números en posiciones aleatorias del tablero sin superposición
+        for num in numeros:
+            while True:
+                posicion = np.random.randint(8*8)
+                fila, columna = np.unravel_index(posicion, (8, 8))
+                if tablero[fila, columna] == 0:
+                    tablero[fila, columna] = num
+                    break
+
+        # Asignar el número 8 en una posición aleatoria para representar el caballo blanco
         while True:
             posicion = np.random.randint(8*8)
             fila, columna = np.unravel_index(posicion, (8, 8))
             if tablero[fila, columna] == 0:
-                tablero[fila, columna] = num
-                if num == 8:
-                    # posicion_caballoB =(columna,fila)
-                    columna_caballoB = columna
-                    fila_caballoB = fila
-                elif num == 9:
-                    posicion_caballoN =(columna,fila)
+                tablero[fila, columna] = 8
+                posicion_caballoB =(columna, fila)
                 break
-    resultados = tablero,columna_caballoB,fila_caballoB,posicion_caballoN            
-    return resultados
 
+        # Asignar el número 9 en una posición aleatoria para representar el caballo negro
+        while True:
+            posicion = np.random.randint(8*8)
+            fila, columna = np.unravel_index(posicion, (8, 8))
+            if tablero[fila, columna] == 0:
+                tablero[fila, columna] = 9
+                posicion_caballoN = (columna, fila)
+                break
 
-def obtener_movimientos_caballo(tablero, fila, columna):
-    movimientos = []
-    
-    movimientos_potenciales = [(fila + 2, columna + 1), (fila + 2, columna - 1),
-                              (fila - 2, columna + 1), (fila - 2, columna - 1),
-                              (fila + 1, columna + 2), (fila + 1, columna - 2),
-                              (fila - 1, columna + 2), (fila - 1, columna - 2)]
-    
-    for movimiento in movimientos_potenciales:
-        nueva_fila, nueva_columna = movimiento
-        if 0 <= nueva_fila < 8 and 0 <= nueva_columna < 8: # No salirse del tablero
-            if tablero[nueva_fila][nueva_columna] not in [8, 9]: # No es un caballo
-                movimientos.append(movimiento)
-    
-    return movimientos
+        return tablero, posicion_caballoB, posicion_caballoN
 
-tablero, columna_caballoB, fila_caballoB,posicion_caballoN = generar_tablero()
-print(tablero,columna_caballoB,fila_caballoB,posicion_caballoN )
+def obtener_movimientos_caballo(posicion_caballo):
+        movimientos = []
+        offsets = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+        
+        for offset in offsets:
+            nueva_columna = posicion_caballo[0] + offset[0]
+            nueva_fila = posicion_caballo[1] + offset[1]
+            
+            if 0 <= nueva_columna < 8 and 0 <= nueva_fila < 8:
+                movimientos.append((nueva_columna, nueva_fila))
+        
+        return movimientos
 
-# Mover el caballo negro
-movimientos = obtener_movimientos_caballo(tablero,fila_caballoB, columna_caballoB)
-print(movimientos)
-
-# # print("Tablero inicial:")
-# print(generar_tablero())
-# print(generar_tablero()[1],generar_tablero()[2])
-# # Mover el caballo negro
-# movimientos = obtener_movimientos_caballo(generar_tablero()[1],generar_tablero()[2])
-# print(movimientos)
-# if movimientos:
-#     nuevo_movimiento = np.random.choice(len(movimientos))
-#     nueva_fila, nueva_columna = movimientos[nuevo_movimiento]
-#     tablero[fila_8, columna_8] = 0  # Limpiar la posición anterior
-#     tablero[nueva_fila, nueva_columna] = 9  # Mover el caballo negro
-#     print("Tablero después del movimiento del caballo negro:")
-#     print(tablero)
-# else:
-#     print("No hay movimientos válidos para el caballo negro.")
+tablero, posicion_caballoB, posicion_caballoN = generar_tablero()
+posibles = obtener_movimientos_caballo(posicion_caballoB)
+print(tablero)
+print (posicion_caballoB)
+print(posibles)
 
 def profundidad(matriz_juego):
     nodos_creados = 0
