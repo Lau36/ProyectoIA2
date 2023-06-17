4f7a84d96d10a73567babd2761e2b06f91c73838
Mayra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2023-06-17 17:18:33 -0500

    interfaz

--- views/ventana_juego.py
+++ views/ventana_juego.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from views.tablero import Tablero
 
 class Ventana_Juego(tk.Toplevel):
     def __init__(self):
         super().__init__()
         self.title("Smart Horses")
-        self.geometry("600x600")
+        self.geometry("600x750")
         self.config(bg="white")
-        
+        self.cell_size = 50  # Size of each cell
+        self.cells = []  # List to store references to the created cells
+
         # Imagen de titulo
         img = Image.open("resources/images/Smart Horses.png")
         img = img.resize((610, 200))  # Resize the image
@@ -45,37 +48,85 @@ class Ventana_Juego(tk.Toplevel):
         self.difficultLabel.place(x=50, y=160)
 
         # Botones de dificultad
-        self.botonPrincipiante = tk.Button(self, image=self.btnPrincipiante_img, bg="white",bd=0 , command= lambda: self.nivel("1"))
+        self.botonPrincipiante = tk.Button(self, image=self.btnPrincipiante_img, bg="white", bd=0, command=lambda: self.nivel("1"))
         self.botonPrincipiante.pack()
-        self.botonPrincipiante.place(x= 250, y= 150)
+        self.botonPrincipiante.place(x=250, y=150)
 
-        self.botonAmateur = tk.Button(self, image=self.btnAmateur_img, bg="white",bd=0, command= lambda: self.nivel("2"))
+        self.botonAmateur = tk.Button(self, image=self.btnAmateur_img, bg="white", bd=0, command=lambda: self.nivel("2"))
         self.botonAmateur.pack()
-        self.botonAmateur.place(x= 370, y= 150)
+        self.botonAmateur.place(x=370, y=150)
 
-        self.botonExperto = tk.Button(self, image=self.btnExperto_img, bg="white",bd=0, command= lambda: self.nivel("3"))
+        self.botonExperto = tk.Button(self, image=self.btnExperto_img, bg="white", bd=0, command=lambda: self.nivel("3"))
         self.botonExperto.pack()
-        self.botonExperto.place(x= 480, y= 150)
+        self.botonExperto.place(x=480, y=150)
 
-        # Canvas de tablero
+        # Canvas of the game board
         self.canvas = tk.Canvas(self, bg='blue')
-        self.canvas.place(x=50, y= 250, width=300, height=330)
+        self.canvas.place(x=50, y=250, width=400, height=400)
 
-        def reiniciar_programa(self):
-            self.destroy()  # Cierra la ventana actual
-            Ventana_Juego()  # Abre una nueva ventana
+        # Create the chessboard using the Tablero class
+        self.cell_size = 50  # Size of each cell
+        self.cells = []  # List to store references to the created cells
+        tablero = Tablero(self.canvas, self.cell_size, self.cells)
+        tablero.crear_tablero()
+
+        # Bind events to the cells
+        for cell in self.cells:
+            self.canvas.tag_bind(cell, "<Button-1>", self.cell_clicked)
 
         # Boton reiniciar
-        self.botonReinicio = tk.Button(self, image=self.btnReinicio_img, bg="white",bd=0, command=reiniciar_programa)
+        self.botonReinicio = tk.Button(self, image=self.btnReinicio_img, bg="white", bd=0, command=self.reiniciar_programa)
         self.botonReinicio.pack()
-        self.botonReinicio.place(x= 420, y= 520)
+        self.botonReinicio.place(x=230, y=680)
 
-    def nivel(self,nivel):
-        if nivel=="1":
+    def cell_clicked(self, event):
+        clicked_cell = event.widget.find_closest(event.x, event.y)
+        cell_index = self.cells.index(clicked_cell[0])
+
+        # Do something with the clicked cell
+        print(f"Cell clicked: {cell_index}")
+
+    def reiniciar_programa(self):
+        self.destroy()  # Cierra la ventana actual
+        Ventana_Juego()  # Abre una nueva ventana
+
+    def nivel(self, nivel):
+        if nivel == "1":
+            # Generate the game board and horse positions
+            tablero, columna_caballoB, fila_caballoB, posicion_caballoN = Tablero.generar_tablero(self.canvas)
+
+            # Clear the canvas
+            self.canvas.delete("all")
+
+            # Create a new chessboard using the Tablero class
+            tablero_obj = Tablero(self.canvas, self.cell_size, self.cells)
+            tablero_obj.crear_tablero()
+
+            # Update the game board with the generated numbers
+            for i in range(8):
+                for j in range(8):
+                    x = j * self.cell_size + self.cell_size // 2
+                    y = i * self.cell_size + self.cell_size // 2
+                    numero = tablero[i][j]
+                    if numero == 8:
+                        img_caballoB = Image.open("resources/images/caballo_blanco.png")
+                        img_caballoB = img_caballoB.resize((40, 40), Image.ANTIALIAS)
+                        self.canvas.image_caballoB = ImageTk.PhotoImage(img_caballoB)
+                        self.canvas.create_image(x, y, image=self.canvas.image_caballoB)
+                    elif numero == 9:
+                        img_caballoN = Image.open("resources/images/caballo_negro.png")
+                        img_caballoN = img_caballoN.resize((40, 40), Image.ANTIALIAS)
+                        self.canvas.image_caballoN = ImageTk.PhotoImage(img_caballoN)
+                        self.canvas.create_image(x, y, image=self.canvas.image_caballoN)
+                    elif numero != 0:
+                        self.canvas.create_text(x, y, text=str(numero), font=("Arial", 12))
+
+            # Do something with the horse positions
+            print(f"Caballo blanco: columna {columna_caballoB}, fila {fila_caballoB}")
+            print(f"Caballo negro: columna {posicion_caballoN[0]}, fila {posicion_caballoN[1]}")
+            print("Tablero: ", tablero)
+        elif nivel == "2":
             pass
-        elif nivel=="2":
-            pass
-        elif nivel=="3":
+        elif nivel == "3":
             pass
 
-    
